07e93fda662669a1df8bafd06076e1ad48d47f38
daikiku10 <soccerdk19.910@gmail.com>
2023-08-02 07:10:28 +0900

    update:#5 サイドメニューの大枠デザインの作成

--- src/src/components/pages/Home/presenter.tsx
+++ src/src/components/pages/Home/presenter.tsx
@@ -1,13 +1,16 @@
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { CustomButton } from '../../uiParts/CustomButton';
+import { SideMenuLayout } from '../../uiParts/SideMenuLayout';
 
 const Home = () => (
-  <Box p={4}>
-    <CustomButton>Aaaaa</CustomButton>
-    <Typography variant="h1">
-      人類社会のすべての構成員の固有の尊厳と平等で譲
-    </Typography>
-  </Box>
+  <SideMenuLayout>
+    <>
+      <CustomButton>Aaaaa</CustomButton>
+      <Typography variant="h1">
+        人類社会のすべての構成員の固有の尊厳と平等で譲
+      </Typography>
+    </>
+  </SideMenuLayout>
 );
 
 export default Home;
--- src/src/components/uiParts/SideMenuLayout/presenter.tsx
+++ src/src/components/uiParts/SideMenuLayout/presenter.tsx
@@ -1,9 +1,59 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@mui/material';
+
+const drawerWidth = 240;
+
+type Props = {
+  children: ReactNode;
+};
+
 /**
  * サイドメニューコンポーネント
  *
  * @param children メインコンテンツ
  * @returns コンポーネント
  */
-const SideMenuLayout = () => <>サイドメニュー </>;
+const SideMenuLayout = ({ children }: Props) => (
+  <Box sx={{ display: 'flex' }}>
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      {children}
+    </Box>
+  </Box>
+);
 
 export default SideMenuLayout;
