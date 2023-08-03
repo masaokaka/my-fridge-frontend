275cf6cc0362fbcbdd277995f7eb0970607ecff2
masaokaka <osushiiisensu@gmail.com>
2023-08-03 21:31:20 +0900

    add: #4 先行してレイアウトをざっくり作成

--- src/src/components/uniqueParts/Layout/index.tsx
+++ src/src/components/uniqueParts/Layout/index.tsx
@@ -1,1 +1,1 @@
-export default {};
+export { default as Layout } from './presenter';
--- src/src/components/uniqueParts/Layout/presenter.tsx
+++ src/src/components/uniqueParts/Layout/presenter.tsx
@@ -1,1 +1,32 @@
-export default {};
+import { Box, Container } from '@mui/material';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
+import { headerHeight } from '../Header/presenter';
+import { footerHeight } from '../Footer/presenter';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const sideNavWidth = 230;
+
+const Layout = ({ children }: Props) => (
+  <Box display="flex">
+    {/* サイドナビがここ */}
+    <Box width={sideNavWidth}>サイドナビ</Box>
+    <Box sx={{ width: `calc(100% - ${sideNavWidth}px)` }}>
+      <Header />
+      <Container
+        sx={{
+          minHeight: `calc(100vh - ${headerHeight + footerHeight}px)`,
+          // TODO: 後で消す
+          outline: 'solid 1px black',
+        }}
+      >
+        {children}
+      </Container>
+      <Footer />
+    </Box>
+  </Box>
+);
+export default Layout;
