63c483e13edcbdc099bbc8237131d19948733e74
daikiku10 <soccerdk19.910@gmail.com>
2023-08-06 10:09:20 +0900

    update:#5 ブランチ4の変更に合わせる

--- src/src/components/pages/Home/presenter.tsx
+++ src/src/components/pages/Home/presenter.tsx
@@ -1,15 +1,10 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { CustomButton } from '../../uiParts/CustomButton';
-import { SideMenuLayout } from '../../uiParts/SideMenuLayout';
 
 const Home = () => (
-  <>
-    <SideMenuLayout />
+  <Box>
+    <Typography variant="h1">メインコンテンツ</Typography>
     <CustomButton>Aaaaa</CustomButton>
-    <Typography variant="h1">
-      人類社会のすべての構成員の固有の尊厳と平等で譲
-    </Typography>
-  </>
+  </Box>
 );
-
 export default Home;
