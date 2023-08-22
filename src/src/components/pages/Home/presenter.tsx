6425996da629cd5ba23aa58e1cd7619105f1bf7a
masaokaka <osushiiisensu@gmail.com>
2023-08-22 12:12:48 +0900

    update: #10 ボタンのUIを実装

--- src/src/components/pages/Home/presenter.tsx
+++ src/src/components/pages/Home/presenter.tsx
@@ -1,10 +1,11 @@
 import { Box, Typography } from '@mui/material';
+import { CalendarMonth } from '@mui/icons-material';
 import { CustomButton } from '../../uiParts/CustomButton';
 
 const Home = () => (
   <Box>
     <Typography variant="h1">メインコンテンツ</Typography>
-    <CustomButton>Aaaaa</CustomButton>
+    <CustomButton text="献立登録" icon={<CalendarMonth />} />
   </Box>
 );
 export default Home;
--- src/src/components/uiParts/CustomButton/presenter.tsx
+++ src/src/components/uiParts/CustomButton/presenter.tsx
@@ -1,21 +1,34 @@
-import { Button, ButtonProps, styled } from '@mui/material';
+import { Button, styled } from '@mui/material';
 import { ReactNode } from 'react';
 
-type Props = ButtonProps & {
-  children?: ReactNode;
+type Props = {
+  /** アイコン */
+  icon?: ReactNode;
+  /** タイプ */
+  type?: 'outlined' | 'contained';
+  /** テキスト */
+  text: string;
 };
 
 const StyledButton = styled(Button)(({ theme }) => ({
   borderRadius: 24,
-  height: 40,
+  height: 45,
+  minWidth: 120,
+  '&.MuiButton-outlinedSecondary': {
+    backgroundColor: '#fff',
+  },
   [theme.breakpoints.up('sm')]: {
     height: 50,
   },
 }));
-
-const CustomButton = ({ children, ...props }: Props) => (
-  <StyledButton {...props} variant="contained">
-    {children}
+/** 共通ボタン
+ * @param icon アイコン
+ * @param text テキスト
+ * @param type タイプ
+ */
+const CustomButton = ({ text, icon, type = 'contained' }: Props) => (
+  <StyledButton variant={type} color="secondary" startIcon={icon}>
+    {text}
   </StyledButton>
 );
 
