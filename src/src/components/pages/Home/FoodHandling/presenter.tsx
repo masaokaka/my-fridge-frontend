b56d81d03790965432b80ff5c1813b0190937ee9
daikiku10 <soccerdk19.910@gmail.com>
2023-09-28 21:02:24 +0900

    update:食材を使うのタイトル部分の作成

--- src/src/components/pages/Home/FoodHandling/presenter.tsx
+++ src/src/components/pages/Home/FoodHandling/presenter.tsx
@@ -6,6 +6,8 @@ import {
   Divider,
   Typography,
 } from '@mui/material';
+import { CalendarMonth } from '@mui/icons-material';
+import { CustomButton } from '../../../uiParts/CustomButton';
 
 /**
  * 食材管理リスト
@@ -20,6 +22,25 @@ const FoodHandlingPresenter = () => (
       </AccordionSummary>
       <AccordionDetails>
         <Divider />
+        <Box
+          marginTop={2}
+          paddingLeft={3}
+          paddingRight={3}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography>2021年11月26日(火)の献立</Typography>
+          <CustomButton
+            startIcon={<CalendarMonth />}
+            variant="contained"
+            onClick={() => console.log('献立表へ遷移する')} // TODO: 押下時処理の実装
+          >
+            献立表
+          </CustomButton>
+        </Box>
         中身
       </AccordionDetails>
     </Accordion>
