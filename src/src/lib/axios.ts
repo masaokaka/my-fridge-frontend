e12f706ff1568085a618fce5ad2dcd9c453830a0
masaokaka <osushiiisensu@gmail.com>
2024-02-12 18:57:59 +0900

    fix: #18 API URLの環境変数を修正

--- src/src/lib/axios.ts
+++ src/src/lib/axios.ts
@@ -1,7 +1,7 @@
-export default {};
-// import Axios, { AxiosRequestConfig } from 'axios';
+import Axios from 'axios';
+// { AxiosRequestConfig }
+import { API_URL } from '../config';
 
-// import { API_URL } from '../config';
 // import { useNotificationStore } from '../stores/notifications';
 // import storage from '../utils/storage';
 
@@ -14,9 +14,9 @@ export default {};
 //   return config;
 // }
 
-// export const axios = Axios.create({
-//   baseURL: API_URL,
-// });
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
 
 // axios.interceptors.request.use(authRequestInterceptor);
 // axios.interceptors.response.use(
